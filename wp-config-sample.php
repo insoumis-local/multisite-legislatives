df1c33628619633e8ed55e16d8619e1bcbf0f990
Nico Prat <nicooprat@gmail.com>
2017-05-13 15:56:48 +0200

    Revert wp-config-sample

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -18,19 +18,14 @@
  */
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
+/** Nom de la base de données de WordPress. */
+define('DB_NAME', '');
 
-switch ($_SERVER['SERVER_NAME']) {
-  case 'circo-01-1':
-    define('DB_NAME', 'circo-01-1');
-    define('DB_USER', 'circo-01-1');
-    define('DB_PASSWORD', 'xxx');
-  break;
-  case 'circo-01-2':
-    define('DB_NAME', 'circo-01-2');
-    define('DB_USER', 'circo-01-2');
-    define('DB_PASSWORD', 'xxx');
-  break;
-}
+/** Utilisateur de la base de données MySQL. */
+define('DB_USER', '');
+
+/** Mot de passe de la base de données MySQL. */
+define('DB_PASSWORD', '');
 
 /** Adresse de l’hébergement MySQL. */
 define('DB_HOST', 'localhost');
